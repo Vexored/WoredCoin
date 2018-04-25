17e146f96f507f213d09116bf04e2880cbcf0609
Vexored <florian.rolland@outlook.com>
2018-04-25 16:33:34 +0200

    Prototype Merkel

---
+++ src/level_1/C/merkel.c
@@ -0,0 +1,37 @@
+#include "merkel.h"
+
+
+//Moteur de hashage
+char* processMerkelRoot(){
+
+  //Creation de la tab pour stocker les hash
+  char** tabHash = malloc(nbTrans * (sizeof*(char*)));
+  for(int i = 0; i < nbTrans: ++i){
+    tabHash[i] = malloc(HASH_SIZE * (sizeoj*(char)));
+  }
+
+  //Hashage de chaque transaction si impair on duplique la dernière case du tableau
+  for(int i = 0; i < nbTrans; ++i){
+
+  }
+  if(nbTrans%2 != 0){
+    //dernière case dupliquer
+  }
+
+  //Calcul des hash comme un génie
+  for(int i = nbTrans/2; i >= 0; i = i/2){
+    for(int j = 0; j < i*2 ; ++j){
+
+    }
+  }
+}
+
+
+
+
+
+
+//Fonction de control
+char* getMerkelRoot(tableau transaction, nb transactions){
+
+}
