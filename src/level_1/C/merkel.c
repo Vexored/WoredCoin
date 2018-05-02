e0a1e9062d3bb6719cf9689b33c1b9b2fb3a9a5d
Vexored <florian.rolland@outlook.com>
2018-05-02 16:14:46 +0200

    Merkle Process writting terminated ! Need compiling and Test.

--- src/level_1/C/merkel.c
+++ src/level_1/C/merkel.c
@@ -1,37 +1,29 @@
 #include "merkel.h"
 
+//BYTE buffer[sizeof(a) + sizeof(b)];
+typedef	unsigned char	BYTE ;	/* 8-bit data */
+//Fonction de control
+char* getMerkelRoot(char** tabTransaction, int nbTransaction){
 
-//Moteur de hashage
-char* processMerkelRoot(){
-
-  //Creation de la tab pour stocker les hash
-  char** tabHash = malloc(nbTrans * (sizeof*(char*)));
-  for(int i = 0; i < nbTrans: ++i){
-    tabHash[i] = malloc(HASH_SIZE * (sizeoj*(char)));
+  char** tabHash = malloc(nbTransaction * (sizeof*(char*)));
+  for(int i = 0; i < nbTransaction: ++i){
+    tabHash[i] = malloc(HASH_SIZE * (sizeof*(char)) + 2);
   }
-
   //Hashage de chaque transaction si impair on duplique la dernière case du tableau
-  for(int i = 0; i < nbTrans; ++i){
-
+  for(int i = 0; i < nbTransaction; ++i){
+    BYTE buffer[sizeof(tabTransaction[i])] = tabTransaction[i];
+    sha256ofString(buffer, tabHash[i]);
   }
-  if(nbTrans%2 != 0){
-    //dernière case dupliquer
+  if(nbTransaction%2 != 0){
+    tabHash[nbTransaction - 1] = tabHash[nbTransaction - 2];
   }
-
   //Calcul des hash comme un génie
-  for(int i = nbTrans/2; i >= 0; i = i/2){
-    for(int j = 0; j < i*2 ; ++j){
-
+  for(int i = nbTransaction/2; i >= 0; i = i/2){
+    for(int j = 0; j < i*2; j = j + 2){
+        BYTE buffer[sizeof(tabHash[j]) + sizeof(tabHash[j+1])];
+        snprintf(buffer, sizeof(buffer), "%s%s", tabHash[j], tabHash[j+1]);
+        sha256ofString(buffer, tabHash[j]);
     }
   }
-}
-
-
-
-
-
-
-//Fonction de control
-char* getMerkelRoot(tableau transaction, nb transactions){
-
+  return tabHash[0];
 }
